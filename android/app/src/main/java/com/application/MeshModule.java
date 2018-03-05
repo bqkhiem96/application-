4932c81ddeace070f130b776e0dcca59243417b1
buiquockhiem <bqkhiem96@gmail.com>
2018-03-05 19:39:25 +0700

    starting to implement hype

--- android/app/src/main/java/com/application/MeshModule.java
+++ android/app/src/main/java/com/application/MeshModule.java
@@ -1,5 +1,9 @@
 package com.application;
 
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -15,12 +19,16 @@ import com.hypelabs.hype.NetworkObserver;
 import com.hypelabs.hype.StateObserver;
 
 import java.io.Console;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Created by bqkhiem96 on 04-Mar-18.
  */
 
 public class MeshModule extends ReactContextBaseJavaModule {
+    ReactApplicationContext context = getReactApplicationContext();
     public MeshModule(ReactApplicationContext reactContext) {
         super(reactContext);
     }
@@ -28,5 +36,26 @@ public class MeshModule extends ReactContextBaseJavaModule {
     public String getName() {
         return "MeshModule";
     }
+
+
+    public void onApplicationStart(Application app) {
+
+        requestHypeToStart();
+    }
+    public void onApplicationStop(Application app) {
+
+        requestHypeToStop();
+    }
+
+    private void requestHypeToStop() {
+    }
+
+    private void requestHypeToStart() {
+
+        Hype.getInstance().setContext(context);
+        Hype.getInstance().addStateObserver((StateObserver) this);
+
+    }
+
 
 }
